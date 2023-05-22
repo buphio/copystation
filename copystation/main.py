f65abe6ce5e7d1a336061eecb0414b939c90ac59
Philipp Buchinger <buchinger@proton.me>
2023-05-22 18:07:15 +0200

    added user config

--- copystation/main.py
+++ copystation/main.py
@@ -4,6 +4,7 @@ POC for copystation backend
 TODO: create sha1sum file in dest folder, then unmount, write to logfile
 """
 
+import configparser
 import logging.config
 from dataclasses import dataclass
 from datetime import datetime
@@ -13,10 +14,14 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
+
 logging.config.fileConfig("logging.conf")
 event_logger = logging.getLogger("events")
 app_logger = logging.getLogger("app")
+
+config = configparser.ConfigParser()
 
 
 @dataclass
@@ -30,7 +35,7 @@ class Device:
 
 def get_device_info(device: str) -> list | None:
     """
-    try to get partition and label of attached block device via 'lsblk'
+    try to get biggest partition (to mount) and label of attached block device via 'lsblk'
     lsblk -n -o SIZE,KNAME,LABEL --bytes /dev/xxx | sort -r | head -2 | tail -1
     TODO: test edge case when no label is given ?
     """
@@ -44,11 +49,13 @@ def get_device_info(device: str) -> list | None:
         sort = run(["sort", "-r"], input=lsblk.stdout, stdout=PIPE, check=True)
         head = run(["head", "-2"], input=sort.stdout, stdout=PIPE, check=True)
         device_info = check_output(["tail", "-1"], input=head.stdout).decode().strip()
+
         if len(device_info.split()) < 2:
             app_logger.critical(f"'{device}' does not contain proper partition")
             return None
+
         return device_info.split()[1:]
-    except CalledProcessError as error:
+    except CalledProcessError:
         app_logger.critical(f"'{device} does not seem to be a proper block device")
         return None
 
@@ -60,12 +67,24 @@ def add_device(name: str) -> None:
     device_info = get_device_info("mmcblk0")
     if not device_info:
         return
+
     device = Device(name, *device_info)
     event_logger.info(f"+++ {datetime.now()} {name} '{device.label}'")
-    # device attached - mount and copy
-    if not mount_device(device):
+
+    mount_point = mount_device(device)
+    if not mount_point:
         return
-    event_logger.info(f"=== {datetime.now()} {name} mounted.")
+    event_logger.info(f"::: {datetime.now()} {name} mounted on {mount_point}")
+
+    # read config
+    config.read("user.conf")
+    project = "{config['project']['name']}-{custom_timestamp('date')}"
+    folder_prefix = device.label if device.label != "" else device.name
+    destination = f"/home/copycat/mounts/{project}/{folder_prefix}-{custom_timestamp('date')}"
+
+    if not copy_files(mount_point, destination):
+        return
+    event_logger.info(f"::: {datetime.now()} {device.label} copied to {destination}")
 
 
 def delete_device(name: str) -> None:
@@ -75,28 +94,55 @@ def delete_device(name: str) -> None:
     device_info = get_device_info("mmcblk0")
     if not device_info:
         return
+
     device = Device(name, *device_info)
     event_logger.info(f"--- {datetime.now()} {name} '{device.label}'")
 
 
-def mount_device(device: Device) -> bool:
-    # create mountpoint
-    folder_prefix = device.label if device.label != "" else device.name
-    mount_point = f"/home/copycat/mounts/{folder_prefix}-{time_formatted()}"
+def mount_device(device: Device) -> str | None:
+    """
+    create mount point from device label or device name and try to mount
+    """
+    # TODO: check mountpoint!!!
+    mount_point = f"/mnt/{device.name}-{custom_timestamp()}"
     try:
-        run(["mkdir", "-p", mount_point], check=True)
+        run(["mkdir", "-p", f"{mount_point}"], check=True)
+    except CalledProcessError:
+        app_logger.critical(f"Could not create '{mount_point}'")
+        return None
+
+    # mount partition
+    try:
+        run(["mount", device.partition, mount_point], check=True)
     except CalledProcessError as error:
         app_logger.critical(error)
-        return False
-    # mount partition
-    # - run(["mount", partition, mount_point], check=True)
+        return None
+
+    return mount_point
 
 
-def create_checksum_file() -> None:
-    pass
+def create_checksum_file(mount_point: str) -> bool:
+    """
+    create file with sha1 checksum of all files in src and copy it to dest folder
+    """
+    # create checksum file
+    try:
+        run(["find", f"{mount_point}", "-type", "f"], check=True)
+    except CalledProcessError:
+        pass
+
+    return True
 
 
-def time_formatted() -> str:
+def copy_files(mount_point: str, destination: str) -> bool:
+    return True
+
+
+def custom_timestamp(format="datetime") -> str:
+    if format == "date":
+        return datetime.now().strftime("%Y%m%d")
+    elif format == "time":
+        return datetime.now().strftime("%H%M%S")
     return datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
@@ -129,3 +175,7 @@ async def device_post(name: str, background_tasks: BackgroundTasks):
 async def device_delete(name: str, background_tasks: BackgroundTasks):
     background_tasks.add_task(delete_device, name)
     return {f"{name}": "removed"}
+
+@app.post("/settings")
+async def set_user_settings():
+    return None
