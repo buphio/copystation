47708c0357bfe9c3fdf39c44c36d0f38ae9d0170
Philipp Buchinger <buchinger@proton.me>
2023-05-31 20:18:04 +0200

    restructuring

---
+++ scripts/devinfo.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+#
+# Author: Philipp Buchinger <me@buph.io>
+#
+# (c) 2023
+#
+
+# general settings
+set -o errexit
+set -o nounset
+set -o pipefail
+
+script_name=$(basename "${0}")
+readonly script_name
+
+
+main() {
+    
+}
+
+main "${0}"
